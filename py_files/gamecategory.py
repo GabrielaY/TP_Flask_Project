d1e6f429db988d8f5c9064e58ac9a9dc0870a31c
Gabriela Yontcheva <gabrielay2002@gmail.com>
2020-01-10 12:44:29 +0200

    Adding files for comments, games, categories and users
    Signed-off-by: Gabriela Yontcheva <gabrielay2002@gmail.com>

---
+++ py_files/gamecategory.py
@@ -0,0 +1,45 @@
+from database import DB
+from game import Game
+
+
+class Category:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    @staticmethod
+    def all():
+        with DB() as db:
+            rows = db.execute('SELECT * FROM gamecategories').fetchall()
+            return [Category(*row) for row in rows]
+
+    @staticmethod
+    def find(id):
+        with DB() as db:
+            row = db.execute('SELECT * FROM gamecategories WHERE id = ?', (id,)) \
+                .fetchone()
+            if not row:
+                return Category(0, "No category")
+            return Category(*row)
+
+    def create(self):
+        with DB() as db:
+            values = (self.name,)
+            db.execute('''
+                INSERT INTO categories (name)
+                VALUES (?)''', values)
+            return self
+
+    def delete(self):
+        with DB() as db:
+            db.execute('DELETE FROM gamecategories WHERE id = ?', (self.id,))
+            db.execute(
+                '''
+                UPDATE games
+                SET category_id = 0 WHERE category_id = ?
+                ''',
+                (self.id,)
+            )
+
+    def games(self):
+        return Game.find_by_category(self)
