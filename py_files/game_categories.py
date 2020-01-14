44d54fc91ef7808f3e270eae398b4d24fcd00fec
Gabriela Yontcheva <gabrielay2002@gmail.com>
2020-01-14 10:26:39 +0200

    Add game categories and updated daba base Signed-off-by: Gabriela Yontcheva <gabrielay2002@gmail.com>

---
+++ py_files/game_categories.py
@@ -0,0 +1,44 @@
+from database import DB
+
+
+
+class Category:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    @staticmethod
+    def all():
+        with DB() as db:
+            rows = db.execute('SELECT * FROM categories').fetchall()
+            return [Category(*row) for row in rows]
+
+    @staticmethod
+    def find(id):
+        with DB() as db:
+            row = db.execute('SELECT * FROM categories WHERE id = ?', (id,)) \
+                .fetchone()
+            if not row:
+                return Category(0, "No category")
+            return Category(*row)
+
+    def create(self):
+        with DB() as db:
+            values = (self.name,)
+            db.execute('''
+                INSERT INTO categories (name)
+                VALUES (?)''', values)
+            return self
+
+    def delete(self):
+        with DB() as db:
+            db.execute('DELETE FROM categories WHERE id = ?', (self.id,))
+            db.execute(
+                '''
+                UPDATE posts
+                SET category_id = 0 WHERE category_id = ?
+                ''',
+                (self.id,)
+            )
+
+
